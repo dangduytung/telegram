570d3bac30627bf5160a6fe6003357d5e1139820
dangduytung <demontendote@gmail.com>
2020-08-05 16:01:46 +0700

    Upload project telegram bot forward

---
+++ bot-forward/index.js
@@ -0,0 +1,41 @@
+// Load env config
+require("dotenv").config();
+
+const fetch = require("node-fetch");
+
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const port = 3001;
+
+// Configurations
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to bot forward");
+});
+
+app.get("/test", (req, res) => {
+  res.send("Test ok");
+});
+
+app.post("/post", (req, res) => {
+  res.send("Post ok");
+});
+
+app.post("/", async (req, res) => {
+  // console.log("BODY", req.body);
+
+  await fetch(process.env.URL_FORWARD, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(req.body),
+  });
+
+  res.send("Forward ok");
+});
+
+// Listening
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
