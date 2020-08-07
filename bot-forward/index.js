7b8d0a0d74efe0236a08263f525dcede6d390b39
dangduytung <demontendote@gmail.com>
2020-08-07 09:40:16 +0700

    Edit

--- bot-forward/index.js
+++ bot-forward/index.js
@@ -24,15 +24,24 @@ app.post("/post", (req, res) => {
 });
 
 app.post("/", async (req, res) => {
-  // console.log("BODY", req.body);
-  console.log(process.env.HEADER_TOKEN)
-  await fetch(process.env.URL_FORWARD, {
-    method: "POST",
-    headers: { "Content-Type": "application/json", "HEADER-TOKEN": process.env.HEADER_TOKEN },
-    body: JSON.stringify(req.body),
-  });
+  try {
+    // console.log("BODY", req.body);
+    // console.log(process.env.HEADER_TOKEN);
+    // console.log(process.env.URL_FORWARD);
+    await fetch(process.env.URL_FORWARD, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "HEADER-TOKEN": process.env.HEADER_TOKEN,
+      },
+      body: JSON.stringify(req.body),
+    });
 
-  res.send("Forward ok");
+    res.send("Forward ok");
+  } catch (error) {
+    console.error(error);
+    res.send("Forward fail");
+  }
 });
 
 // Listening
